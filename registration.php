6927c964b7a63e3a3bcd42a7dbf720e8b043b415
raju_icreon <raju.sadadiya@icreon.com>
2021-08-19 17:22:18 +0530

    Add base module changes

--- registration.php
+++ registration.php
@@ -1,6 +1,12 @@
 <?php
+/**
+ * @author Bytes Technolab Team
+ * @copyright Copyright (c) 2021 Bytes Technolab (https://www.bytestechnolab.com/)
+ * @package BytesTechnolab_Base
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Magebytes_Base',
-	__DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'BytesTechnolab_Base',
+    __DIR__
 );
